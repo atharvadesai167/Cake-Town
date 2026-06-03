4d50d2e4431c1d74f744f4dcac1c080998b27f8b
Atharva Desai <atharva.desai167@gmail.com>
2026-06-03 17:26:35 +0530

    Add lucide-react dependency and enhance Navbar component with mobile menu functionality

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,9 @@
+import { MenuIcon } from "lucide-react";
+import { useState } from "react";
+import { RxCross2 } from "react-icons/rx";
+
 export default function Navbar() {
+    const [mobileMenuIsOpen, setMobileMenuIsOpen] = useState(false);
     return (
         <nav className="fixed top-0 w-screen z-50 transition-all duration-300 bg-white backdrop-blur-sm">
             <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
@@ -24,7 +29,7 @@ export default function Navbar() {
                     </div>
 
                     {/* Nav Links */}
-                    <div className="ml-auto flex items-center space-x-6 lg:space-x-8">
+                    <div className="hidden md:flex items-center space-x-6 lg:space-x-8">
                         <a href="#Home" className="text-black hover:text-orange-400 text-sm lg:text-base">
                             Home
                         </a>
@@ -38,11 +43,38 @@ export default function Navbar() {
                         </a>
                     </div>
 
-                    <button>
+                    <button className="md:hidden p-2 text-gray-700 rounded-md focus:outline-none focus:ring-2 focus:ring-inset focus:ring-orange-400" onClick={() => setMobileMenuIsOpen((prev) => !prev)}>
+                        {mobileMenuIsOpen ? (<RxCross2 className="h-6 w-6 cursor-pointer" aria-hidden="true" />) : (
+                        <MenuIcon className="h-6 w-6 cursor-pointer" aria-hidden="true" />
+                        )
+                        }
                     </button>
-
                 </div>
             </div>
+
+            {mobileMenuIsOpen && (
+            <div className="md:hidden bg-white-100/95 backdrop-blur-lg border-t border-gray-200 animate-in slide-in-from-top duration-300">
+                <div className="px-4 py-4 sm:px-6 space-y-3 sm:space-y-4">
+
+                    <a href="#Home" 
+                    onClick={() => setMobileMenuIsOpen(false)}
+                    className="block text-black hover:text-orange-400 text-sm lg:text-base">
+                            Home
+                        </a>
+
+                        <a href="#About"
+                        onClick={() => setMobileMenuIsOpen(false)}
+                         className="block text-black hover:text-orange-400 text-sm lg:text-base">
+                            About
+                        </a>
+
+                        <a href="#Testimonials" 
+                        onClick={() => setMobileMenuIsOpen(false)}
+                        className="block text-black hover:text-orange-400 text-sm lg:text-base">
+                            Testimonials
+                        </a></div>
+                </div>
+                )}
         </nav>
     );
-}   
+}
