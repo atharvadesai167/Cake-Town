b6c1b9fbc4d60c9e3a98874b49b9d2cb93400150
Atharva Desai <atharva.desai167@gmail.com>
2026-06-03 23:46:10 +0530

    Hero section initial steps

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,3 +1,7 @@
 export default function Hero() {
-    return <div></div>;
+    return (
+     <section className="relative min-h-screen flex items-center justify-center pt-16 sm:pt-20 px-4 sm:px-6 lg:px-8 overflow-hidden">
+
+    </section>
+    );
 }
